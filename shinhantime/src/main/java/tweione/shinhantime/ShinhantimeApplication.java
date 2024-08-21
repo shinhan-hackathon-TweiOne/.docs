d6f9f1775ba3a77875905c630f2e9ac621cb5132
이상미 <dltkdal1230@naver.com>
2024-08-21 10:22:29 +0900

    ADD : spring boot project 추가

---
+++ shinhantime/src/main/java/tweione/shinhantime/ShinhantimeApplication.java
@@ -0,0 +1,13 @@
+package tweione.shinhantime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhantimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinhantimeApplication.class, args);
+	}
+
+}
---
+++ shinhantime/src/test/java/tweione/shinhantime/ShinhantimeApplicationTests.java
@@ -0,0 +1,13 @@
+package tweione.shinhantime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinhantimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
